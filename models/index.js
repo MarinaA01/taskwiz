6fd4566944b694609543afedafe10948f566a48e
Lisa <143395934+Ev3ryTh1ngN1c3@users.noreply.github.com>
2024-02-10 12:46:26 -0500

    fixed the casing of variable in association

--- models/index.js
+++ models/index.js
@@ -1,15 +1,15 @@
 const User = require('./user');
-const Task = require('./Task');
+const Task = require('./task');
 
-// user has many Tasks
+// user has many tasks
 User.hasMany(Task, {
   foreignKey: 'user_id', // foreign key to associate user with the task
-  onDelete: 'CASCADE' // if User is deleted, also delete associated tasks
+  onDelete: 'CASCADE', // if User is deleted, also delete associated tasks
 });
 
 // task belongs to user
 Task.belongsTo(User, {
-  foreignKey: 'user_id' // foreign key to associate the task with user
+  foreignKey: 'user_id', // foreign key to associate the task with user
 });
 
 module.exports = { User, Task };
