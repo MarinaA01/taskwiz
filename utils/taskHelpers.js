a5e6ceaa206595c9bc8a9d4edacae82a012f9f3c
Lisa <143395934+Ev3ryTh1ngN1c3@users.noreply.github.com>
2024-02-10 12:18:15 -0500

    added event handlers for task functionality

---
+++ utils/taskHelpers.js
@@ -0,0 +1,26 @@
+module.exports = {
+    createTask: (e) => {
+      // handle click event for creating a task
+    },
+    handleKeyPress: (e) => {
+      // handle keypress event that allows task to be created on 'enter'
+    },
+    completeTask: (e) => {
+      // handle click event to change state of task to completed
+    },
+    deleteTask: (e) => {
+      // handle click event for deleting uncompleted events
+    },
+    deleteCompletedTask: (e) => {
+      // handle click event for completed events that bypasses the confirm
+    },
+    loadEditTaskPage: (e) => {
+      // handle click event to load the edit task page
+    },
+    submitEditTask: (e) => {
+      // handle click event to submit the edit to the back end
+    },
+    handleEditKeyPress: (e) => {
+      // handle keypress event that allows task to be edited on 'enter'
+    },
+  };
