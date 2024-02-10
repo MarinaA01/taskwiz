fe372e9544d3fcae53a74b5573c30030cf4ff0ef
Lisa <143395934+Ev3ryTh1ngN1c3@users.noreply.github.com>
2024-02-10 14:27:53 -0500

    refactored code to remove circular dependency between models

--- models/User.js
+++ models/User.js
@@ -1,63 +1,69 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model } = require('sequelize');
 const bcrypt = require('bcrypt');
-const sequelize = require('../config/connection');
 
-class User extends Model {
-  // model definition goes here
+module.exports = (sequelize, DataTypes) => {
+  // defining the user model as a subclass of the Sequelize Model class
+  class User extends Model {
+    // model definition goes here
 
-  // method to check if provided password matches the user's hashed password
-  checkPassword(loginPw) {
-    return bcrypt.compareSync(loginPw, this.password);
+    // method to check if provided password matches the user's hashed password
+    checkPassword(loginPw) {
+      return bcrypt.compareSync(loginPw, this.password);
+    }
   }
-}
 
-User.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true,
+  // initializing the User model with the defined attributes & options
+  User.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [8],
+        },
       },
     },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [8],
+    {
+      hooks: {
+        // hash the password before creating a new user
+        beforeCreate: async (newUserData) => {
+          newUserData.password = await bcrypt.hash(newUserData.password, 10);
+          return newUserData;
+        },
+        // hash the password before updating an existing user
+        beforeUpdate: async (updatedUserData) => {
+          updatedUserData.password = await bcrypt.hash(
+            updatedUserData.password,
+            10
+          );
+          return updatedUserData;
+        },
       },
-    },
-  },
-  {
-    hooks: {
-      // hash the password before creating a new user
-      beforeCreate: async (newUserData) => {
-        newUserData.password = await bcrypt.hash(newUserData.password, 10);
-        return newUserData;
-      },
-      // hash the password before updating an existing user
-      beforeUpdate: async (updatedUserData) => {
-        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-        return updatedUserData;
-      },
-    },
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'user',
-  }
-);
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'user',
+    }
+  );
 
-module.exports = User;
+  return User;
+};
--- models/index.js
+++ models/index.js
@@ -1,15 +1,20 @@
-const User = require('./user');
-const Task = require('./task');
+// exporting a function that takes in the sequelize & DataTypes parameters
+module.exports = (sequelize, DataTypes) => {
+  // importing the user & task models, passing sequelize & DataTypes as arguments
+  const User = require('./user')(sequelize, DataTypes);
+  const Task = require('./task')(sequelize, DataTypes);
 
-// user has many tasks
-User.hasMany(Task, {
-  foreignKey: 'user_id', // foreign key to associate user with the task
-  onDelete: 'CASCADE', // if User is deleted, also delete associated tasks
-});
+  // defining the association: user has many tasks
+  User.hasMany(Task, {
+    foreignKey: 'user_id', // foreign key to associate user with the task
+    onDelete: 'CASCADE', // if User is deleted, also delete associated tasks
+  });
 
-// task belongs to user
-Task.belongsTo(User, {
-  foreignKey: 'user_id', // foreign key to associate the task with user
-});
+  // defining the association: task belongs to user
+  Task.belongsTo(User, {
+    foreignKey: 'user_id', // foreign key to associate the task with user
+  });
 
-module.exports = { User, Task };
+  // returning an object that contains the user & task models
+  return { User, Task };
+};
