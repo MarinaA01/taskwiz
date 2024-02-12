49ab3cb0e4e288848fadf88aa5d0d2b9cf781612
Lisa <143395934+Ev3ryTh1ngN1c3@users.noreply.github.com>
2024-02-12 16:31:06 -0500

    added route for tasks for a specific user

--- routes/home.js
+++ routes/home.js
@@ -3,23 +3,23 @@ const router = express.Router();
 const db = require("../models");
 const Task = db.Task;
 
-// handle GET request to retrieve tasks for the current user
+// route to fetch tasks for a specific user
 router.get("/", (req, res) => {
-  // get the UserId from the authenticated user
+  // get the UserId of the authenticated user
   const UserId = req.user.id;
 
-  // find all tasks associated with the UserId
+  // find all tasks for the specific user
   return Task.findAll({
     raw: true,
     nest: true,
     where: { User_id: UserId },
   })
     .then((tasks) => {
-      // render the 'index' view with the retrieved tasks
+      // render the 'index' view with the fetched tasks
       return res.render("index", { tasks: tasks });
     })
     .catch((error) => {
-      // handle any errors & return a status code
+      // handle any errors that occur during the process
       return res.status(422).json(error);
     });
 });
