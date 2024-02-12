99e75fd490f0473b7e9e7eddabb9d4c9a96b042b
Lisa <143395934+Ev3ryTh1ngN1c3@users.noreply.github.com>
2024-02-12 16:15:16 -0500

    added authentication middleware to restrict access to users only

---
+++ todo-sequelize-main/middleware/auth.js
@@ -0,0 +1,10 @@
+module.exports = {
+    authenticator: (req, res, next) => {
+      if (req.isAuthenticated()) {
+        return next()
+      }
+      req.flash('warning_msg', 'Please login first.')
+      res.redirect('/users/login')
+    }
+  }
+  
