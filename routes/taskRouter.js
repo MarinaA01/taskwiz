6d2d11cb6196f2ef33c1e192d93d9d5948eb3bbf
Lisa <143395934+Ev3ryTh1ngN1c3@users.noreply.github.com>
2024-02-12 15:39:04 -0500

    added routes for creating, reading, updating & deleting tasks

--- routes/taskRouter.js
+++ routes/taskRouter.js
@@ -1,17 +1,106 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../models");
+const Task = db.Task;
 
-// import route handlers for different routes
-const home = require("./home");
-const users = require("./UserRouter");
-const tasks = require("./taskRouter");
-const auth = require("./authRouter");
-const { authenticator } = require("../middleware/auth");
+// create a new task form route
+router.get("/new", (req, res) => {
+  res.render("create");
+});
 
-// define route handling for different paths using middleware
-router.use("/tasks", authenticator, tasks); // route for tasks with authentication
-router.use("/users", users); // route for users
-router.use("/auth", auth); // route for authentication
-router.use("/", authenticator, home); // default route with authentication
+// read all tasks route
+router.get("/", (req, res) => {
+  const UserId = req.user.id;
+  console.log(UserId);
+  return Task.findAll({
+    raw: true,
+    nest: true,
+    where: { user_id: UserId },
+  })
+    .then((tasks) => {
+      console.log(tasks);
+      return res.render("index", { tasks: tasks });
+    })
+    .catch((error) => {
+      return res.status(422).json(error);
+    });
+});
+
+// create a new task route
+router.post("/", (req, res) => {
+  const { name, description } = req.body;
+  const user_id = req.user.id;
+  console.log("name:", name);
+  console.log("description:", description);
+  console.log("user_id:", user_id);
+  
+  return Task.create({
+    name,
+    description,
+    user_id,
+    date_created: new Date(),
+  })
+    .then(() => res.redirect("/"))
+    .catch((error) => {
+      console.log("Error creating task:", error);
+      res.status(500).send("Internal Server Error");
+    });
+});
+
+// read a specific task route
+router.get("/:id", (req, res) => {
+  const user_id = req.user.id;
+  const id = req.params.id;
+  
+  return Task.findOne({ where: { user_id, id } })
+    .then((task) => res.render("taskDetails", { task: task.toJSON() }))
+    .catch((error) => console.log(error));
+});
+
+// update a task route
+router.get("/:id/edit", (req, res) => {
+  const UserId = req.user.id;
+  const id = req.params.id;
+  
+  return Task.findOne({ where: { UserId, id } })
+    .then((task) => res.render("editTask", { task: task.toJSON() }))
+    .catch((error) => console.log(error));
+});
+
+router.put("/:id", (req, res) => {
+  const user_id = req.user.id;
+  const id = req.params.id;
+  const { name, isDone } = req.body;
+  
+  return Task.update(
+    {
+      name,
+      isDone: isDone === "on",
+    },
+    {
+      where: {
+        user_id,
+        id,
+      },
+    }
+  )
+    .then(() => res.redirect(`/tasks/${id}`))
+    .catch((error) => console.log(error));
+});
+
+// delete a task route
+router.delete("/:id", (req, res) => {
+  const user_id = req.user.id;
+  const id = req.params.id;
+  
+  return Task.destroy({
+    where: {
+      user_id,
+      id,
+    },
+  })
+    .then(() => res.redirect("/"))
+    .catch((error) => console.log(error));
+});
 
 module.exports = router;
