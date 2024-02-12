697ce4014a7c376392ae21f93361a90e5e7aabc0
Lisa <143395934+Ev3ryTh1ngN1c3@users.noreply.github.com>
2024-02-12 15:05:27 -0500

    implemented login & signup handlers with API requests & redirection logic

--- public/js/login.js
+++ public/js/login.js
@@ -1,56 +1,58 @@
- // login form handler
+// login form handler
 const loginFormHandler = async (event) => {
-    event.preventDefault();
-  
-    // collect values from the login form
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-  
-    if (email && password) {
-      // send a POST request to the API endpoint
-      const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        // if successful, redirect the browser to the profile page
-        document.location.replace('/profile');
-      } else {
-        alert(response.statusText);
-      }
+  event.preventDefault();
+
+  // collect values from the login form
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (email && password) {
+    // send a POST request to the API endpoint
+    const response = await fetch('/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      // if successful, redirect the browser to the profile page
+      document.location.replace('/profile');
+    } else {
+      alert(response.statusText);
     }
-  };
-  
-  // signup form handler
-  const signupFormHandler = async (event) => {
-    event.preventDefault();
-  
-    // collect values from the signup form
-    const name = document.querySelector('#name-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (name && email && password) {
-      // send a POST request to the API endpoint
-      const response = await fetch('/api/users', {
-        method: 'POST',
-        body: JSON.stringify({ name, email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        // if successful, redirect the browser to the profile page
-        document.location.replace('/profile');
-      } else {
-        alert(response.statusText);
-      }
+  }
+};
+
+// signup form handler
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  // collect values from the signup form
+  const name = document.querySelector('#name-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (name && email && password) {
+    // send a POST request to the API endpoint
+    const response = await fetch('/signup', {
+      method: 'POST',
+      body: JSON.stringify({ name, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      // if successful, redirect the browser to the profile page
+      document.location.replace('/profile');
+    } else {
+      alert(response.statusText);
     }
-  };
-  
-  // add event listeners to the login & signup forms
-  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
-  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
-  
-  
+  }
+};
+
+// add event listeners to the login & signup forms
+document
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
